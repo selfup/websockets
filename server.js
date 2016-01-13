8362adf1ce2abade1e85a2e9fe06baf20d5b6289
selfup <boudinot.regis@yahoo.com>
2016-01-13 09:29:24 -0700

    got connected user count to work

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 // server.js
-const http = require('http');
-const express = require('express');
+const http = require('http')
+const express = require('express')
+const socketIo = require('socket.io')
+const app = express()
 
-const app = express();
+app.use(express.static('public'))
 
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
 
-app.get('/', function (req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const server = http.createServer(app)
-                 .listen(port, function () {
-                    console.log('Listening on port ' + port + '.');
-                  });
-module.exports = server;
+                 .listen(port, () => {
+                    console.log('Listening on port ' + port + '.')
+                  })
+
+const io = socketIo(server)
+
+io.on('connection', (socket) => {
+  console.log('A user has connected.', io.engine.clientsCount)
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount)
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected.', io.engine.clientsCount)
+    io.sockets.emit('usersConnected', io.engine.clientsCount)
+  })
+})
+
+module.exports = server
